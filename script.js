e8784d843d97305925bc63d619a3dc12476d9f5c
jurcekk <jurcekdavid@gmail.com>
2024-05-24 17:33:48 +0200

    Added animation

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-document
-  .getElementById('contact-form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Message sent successfully!');
-    window.open(
-      'mailto:jurcekdavid@gmail.com? subject=Message%20from%20' +
-        document.getElementById('name').value +
-        '&body=' +
-        document.getElementById('text').value
-    );
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll(
+    '.features, .screenshots, .download, .contact'
+  );
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in');
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  sections.forEach((section) => {
+    observer.observe(section);
   });
+
+  document
+    .getElementById('contact-form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault();
+      alert('Message sent successfully!');
+      window.open(
+        'mailto:jurcekdavid@gmail.com? subject=Message%20from%20' +
+          document.getElementById('name').value +
+          '&body=' +
+          document.getElementById('text').value
+      );
+    });
+});
